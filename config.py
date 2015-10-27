55543b7da86e2877416b3f96840aa55cbad3f54e
Shawn <shawntaylor@weecology.org>
2015-10-27 07:08:44 -0400

    updated documentation in config.py

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
+#The term schedule that gets displayed. Can only do one at a time.
+#Term codes are YYYYXX, where XX is 01 for spring, 08 for fall, and 06 for summer
 currentTermName='201601'
 
 majorTemplate='in/majorPage.html.mako'
 
+#Add new majors here.
+#Name: short name for the major
+#classFile: the csv file containing all the classes in this majors curriculum
+#asof: the date that the major curriculum was aquired
 majors=[
      {'name': 'SNRE', 'classFile': 'majorClassLists/SNREList.csv', 'asof': 'Oct 10,2015'},
      {'name': 'WEC', 'classFile': 'majorClassLists/WECList.csv', 'asof': 'Oct 10,2015'}
 ]
 
+#Add new semesters here.
+#Name: the code for the year and semester. YYYYXX, where XX is 01 for spring, 08 for fall, and 06 for summer
+#prettyName: the more comprehendable name. eg. Fall 2015
+#termSchedule: the filename for the downloaded csv file for the schedule. All should be semesterData/YYYYXX.csv
 terms=[
      {'name' :'201601', 'prettyName':'Spring 2016', 'termSchedule': 'semesterData/201601.csv'}, 
      {'name' :'201508', 'prettyName':'Fall 2015', 'termSchedule': 'semesterData/201508.csv'}, 
@@ -22,8 +32,12 @@ terms=[
      {'name' :'201008', 'prettyName':'Fall 2010', 'termSchedule': 'semesterData/201008.csv'} 
 ]
 
-#Every dept has 'Special Topic' codes that we may need to petition to take.
-#Theres probably a better way to do this. maybe scrap the grad catalog website
+#Every dept has 'Special Topic' codes that are not necessarily in the curriculum. 
+#Since they all share the same course codes with things thare *are* in the curriclum, 
+#all special topics are included.
+#This list is to go and find them and mark them "special topics" to indicate the class might
+#need prior approval. 
+#Theres probably a better way to account for these. maybe scrape the grad catalog website
 specialTopicClasses=['ZOO6927',
                      'WIS6934',
                      'SWS6932',
