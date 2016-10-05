269ffee35992bd3a4b74f0fe3db53464207127af
Shawn <shawntaylor@weecology.org>
2016-10-05 06:54:27 -0400

    aggregate semester info and write to json

--- compileSearchTerms.py
+++ compileSearchTerms.py
@@ -1,24 +1,38 @@
+#Make a list of every graduate class ever offered and the
+#terms which it was offered in
+
 import pandas as pd
+import json
 from config import *
 
 allClasses=pd.DataFrame(columns=['coursePrefix','courseNum','title','semesters'])
 
-for thisSemester in terms[1:5]:
+for thisSemester in terms:
     thisSemesterData=pd.read_csv(thisSemester['termSchedule'])
     thisSemesterData=thisSemesterData[['coursePrefix','courseNum','title']]
     
-    print(thisSemesterData.shape)
     thisSemesterData=thisSemesterData.drop_duplicates(subset=['coursePrefix','courseNum','title'])
-    print(thisSemesterData.shape)
 
     #Remove trailing letters from course numbers to filter out undergraduate classes
     thisSemesterData['courseNumStrict']=thisSemesterData['courseNum'].str.replace(r'[a-zA-Z]','').astype('int')
     thisSemesterData=thisSemesterData[thisSemesterData.courseNumStrict>=5000]
     thisSemesterData.drop('courseNumStrict', axis=1, inplace=True)
-    print(thisSemesterData.shape)
+
     thisSemesterData['semesters']=thisSemester['prettyName']
 
     allClasses=allClasses.append(thisSemesterData)
 
-print(allClasses.shape)
-print(allClasses)
+#Combine entries for each class into a single line
+def combineSemesters(classes):
+    semestersSingleString=', '.join(classes.tolist())
+    return(semestersSingleString)
+allClasses=allClasses.groupby(['coursePrefix','courseNum','title']).agg(combineSemesters).reset_index()
+
+#Convert to dict for writing out as json objet
+allClasses=allClasses.to_dict('records')
+
+allClasses={'data' : allClasses}
+objectFile='classSearchDB.txt'
+with open(objectFile, 'w') as f:
+    json.dump(allClasses, f, indent=4)
+
