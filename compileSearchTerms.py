5489aabf6debdf00f652513b30a253238441a066
Shawn <shawntaylor@weecology.org>
2016-10-05 05:54:28 -0400

    iterate thru all semesters and find unique classes

---
+++ compileSearchTerms.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from config import *
+
+allClasses=pd.DataFrame(columns=['coursePrefix','courseNum','title','prof','semesters'])
+
+for thisSemester in terms[1:5]:
+    thisSemesterData=pd.read_csv(thisSemester['termSchedule'])
+    thisSemesterData=thisSemesterData[['coursePrefix','courseNum','title','prof']]
+    
+    print(thisSemesterData.shape)
+    thisSemesterData=thisSemesterData.drop_duplicates(subset=['coursePrefix','courseNum','title'])
+    print(thisSemesterData.shape)
+
+    #Remove trailing letters from course numbers to filter out undergraduate classes
+    thisSemesterData['courseNumStrict']=thisSemesterData['courseNum'].str.replace(r'[a-zA-Z]','').astype('int')
+    thisSemesterData=thisSemesterData[thisSemesterData.courseNumStrict>=5000]
+    thisSemesterData.drop('courseNumStrict', axis=1, inplace=True)
+    print(thisSemesterData.shape)
+    thisSemesterData['semesters']=thisSemester['prettyName']
+
+    allClasses=allClasses.append(thisSemesterData)
+
+print(allClasses.shape)
+print(allClasses)
