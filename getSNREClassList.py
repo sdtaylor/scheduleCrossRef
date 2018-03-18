9458dfc0ffc172850cbb7306e2e3f70e4f52650c
Shawn Taylor <shawntaylor@weecology.org>
2018-03-18 15:00:22 -0400

    update scraper to new SNRE website

--- getSNREClassList.py
+++ getSNREClassList.py
@@ -1,11 +1,19 @@
 import urllib.request as urllib
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import os.path
-from config import *
+import os
 
-baseURL='http://snre.ufl.edu/graduate/curriculum.htm'
+baseURL='http://snre.ifas.ufl.edu/academics/graduate/courses-syllabi-and-curriculum/'
 classListFile='majorClassLists/SNREList.csv'
+
+html_titles = ['Principles of Ecology Courses','Particular Perspectives and Systems Ecology Courses',
+               'Natural Science Courses','Social Sciences Courses',
+               'Sustainability Studies Courses','Research and Design Methods Courses']
+short_names = ['Principles of Ecology', 'Particular Systems', 'Natural Science',
+               'Social Science', 'Sustainability', 'Research & Design']
+
+catagories = pd.DataFrame({'html_title':html_titles,'subCatagory':short_names})
+
 
 #Only run if this datafile doesn't exist
 if os.path.exists(classListFile):
@@ -14,65 +22,51 @@ if os.path.exists(classListFile):
 
 pageSoup=bs(urllib.urlopen(baseURL), 'lxml')
 
-#find the different class catagories. Priciples of Ecology, Particular Perspectives, etc
-def isSubCatagory(tag):
-    return tag.name=='div' and 'id' in tag.attrs \
-            and 'fragment' in tag.attrs['id']
+################################################
+# functions defining different html sections for
+# use with beautifulsoup
 
-#Within each subcatagory, find the rows (tr) that are actuall class listings.
-#They will have more than 2 <td> entries
-def isClassListing(tag):
-    return tag.name=='tr' and len(tag.find_all('td'))>2
+def is_div_row(tag):
+    return tag.name=='div' and 'class' in tag.attrs and 'row' in tag.attrs['class']
 
-classList=pd.DataFrame(columns=['coursePrefix','courseNum','title','subCategory'])
+# A div element for one of the 6 class catagories. They are <div class='row'
+# and contain the text defined above
+def is_catagory_section(tag):
+    if is_div_row(tag):
+        title = tag.find('h3')
+        if title:
+            return title.text in catagories.html_title.tolist()
+    else:
+        return False
 
-#
-#Setup subcategories with curriculum. 
-subCategoryList=['Principles of Ecology',
-        'Particular Systems',
-        'Natural Science',
-        'Social Science',
-        'Sustainability',
-        'Research & Design']
+# Class rows are 'tr' elements with exactly 4 'td' elements
+def is_class_listing(tag):
+    if tag.name=='tr':
+        return len(tag.find_all('td')) == 4
+    else:
+        return False
 
+######################################################
+# Primary scraping code
+class_list = []
 
-subCategoryCount=0
-for sub in pageSoup.find_all(isSubCatagory):
-    thisSubCategory=subCategoryList[subCategoryCount]
-    subCategoryCount+=1
-    for classListing in sub.find_all(isClassListing):
-        classEntry={}
-        classListing=classListing.find_all('td')
+for catagory_section in pageSoup.find_all(is_catagory_section):
+    html_title = catagory_section.find('h3').text
+    subCatagory = catagories['subCatagory'][catagories.html_title==html_title].tolist()[0]
+    
+    for class_listing in catagory_section.find_all(is_class_listing):
+        prefix_and_number = class_listing.find_all('td')[0].text
+        title = class_listing.find_all('td')[1].text.strip()
+        
+        prefix = prefix_and_number.split(' ')[0].strip()
+        number = prefix_and_number.split(' ')[1].strip()
+        
+        class_list.append({'coursePrefix':prefix,
+                           'courseNum':number,
+                           'title':title,
+                           'subCategory':subCatagory})
 
-        #Some one off entries that need special handling
-        if '6215L' in classListing[0].get_text():
-            classListing[0].string='ENV 6215/ENV 6215L'
-        if '4043C' in classListing[0].get_text():
-            classListing[0].string='PCB 4043C'
-        #Some entries have two course numbers for one class, seperated by a /
-        #If thats the case, put the 2nd on in the list, then take it out and add
-        #the first one as normal
-        if '/' in classListing[0].get_text():
-            #Add the 2nd entry to the list
-            secondEntry={}
-            secondEntry['coursePrefix']=classListing[0].get_text().split('/')[1].split(' ')[0]
-            secondEntry['courseNum']=classListing[0].get_text().split('/')[1].split(' ')[1]
-            secondEntry['title']=classListing[1].get_text()
-            classList=classList.append(secondEntry, ignore_index=True)
-            #Remove the 2nd entry so the 1st gets added below
-            classListing[0].string=classListing[0].get_text().split('/')[0]
-
-        classEntry['coursePrefix']=classListing[0].get_text().split(' ')[0]
-        classEntry['courseNum']=classListing[0].get_text().split(' ')[1]
-        classEntry['title']=classListing[1].get_text()
-        classEntry['subCategory']=thisSubCategory
-
-        #Do not use law school classes. There is a ton of them and no ecology student should
-        #be taking law classes anyway
-        if classEntry['coursePrefix']=='LAW':
-            break
-
-        classList=classList.append(classEntry, ignore_index=True)
+class_list = pd.DataFrame(class_list)
 
 ############################
 #Some class have multiple sub catagories. Go thru and make one row per class
@@ -80,18 +74,18 @@ for sub in pageSoup.find_all(isSubCatagory):
 
 #There are duplicate rows where the only difference is the subcategory. First find
 #all unique rows.
-classListTemp=classList[['coursePrefix','courseNum','title']].drop_duplicates()
+class_list_temp=class_list[['coursePrefix','courseNum','title']].drop_duplicates()
 #Initialize a subCategory for the unique rows
-classListTemp['subCategory']=''
+class_list_temp['subCategory']=''
 #Go thru row by row and pull the subCategories out, combining them where there are multiple
-for index, row in classListTemp.iterrows():
+for index, row in class_list_temp.iterrows():
     #pull out the subcategories in a list
-    subCats=classList['subCategory'][classList['title']==row['title']].drop_duplicates().tolist()
+    subCats=class_list['subCategory'][class_list['title']==row['title']].drop_duplicates().tolist()
     #Clear any nan values that sneak in
     subCats=[x for x in subCats if str(x) != 'nan']
     #Combine them in a string and put them in the temp dataframe
     row['subCategory']=','.join(subCats)
+    
+class_list = class_list_temp
 
-classList=classListTemp
-
-classList.to_csv(classListFile, index=False)
+class_list.to_csv(classListFile, index=False)
